97746c679d37c228088c90ab768f0552ce71732e
miguel0 <miguelbarroe@gmail.com>
2019-07-16 14:57:32 -0700

    Butterknife added

--- app/src/main/java/com/example/team_project/MainActivity.java
+++ app/src/main/java/com/example/team_project/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.team_project;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    // find fields automatically by id using Butterknife
+    @BindView(R.id.toolbar_main) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         // set toolbar as action bar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
     }
 }
