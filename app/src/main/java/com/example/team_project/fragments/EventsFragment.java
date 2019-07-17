8feaae934f5838f1477e330e18035caf262a6a65
Shannon Jordan <shannonmj@fb.com>
2019-07-16 17:04:35 -0700

    add events layout

--- app/src/main/java/com/example/team_project/fragments/EventsFragment.java
+++ app/src/main/java/com/example/team_project/fragments/EventsFragment.java
@@ -1,18 +1,27 @@
 package com.example.team_project.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
 import com.example.team_project.R;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class EventsFragment extends Fragment {
     private Unbinder unbinder;
+
+    Context context;
+    SearchView svSearch;
+    RecyclerView rvEvents;
+
 
     @Nullable
     @Override
