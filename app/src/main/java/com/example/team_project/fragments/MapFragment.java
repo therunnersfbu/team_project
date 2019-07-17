2b9bbc1500cd36b3635b3ca72736fb56666d8439
Shannon Jordan <shannonmj@fb.com>
2019-07-16 19:18:29 -0700

    working with google map

--- app/src/main/java/com/example/team_project/fragments/MapFragment.java
+++ app/src/main/java/com/example/team_project/fragments/MapFragment.java
@@ -1,5 +1,6 @@
 package com.example.team_project.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,11 +9,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.team_project.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     private Unbinder unbinder;
+    public MapFragment map;
+    Context context;
+    private MapView mapView;
+    private GoogleMap googleMap;
 
     @Nullable
     @Override
@@ -20,17 +31,35 @@ public class MapFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         unbinder = ButterKnife.bind(this, view);
         return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
 
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+       /* MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(getContext()); */
+
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);//when you already implement OnMapReadyCallback in your fragment
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap = map;
     }
 }
