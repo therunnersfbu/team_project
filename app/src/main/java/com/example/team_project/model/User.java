7655321469883d6b3de941f7ffdfdd938a15d965
Sophia Zambri <sophiazambri@fb.com>
2019-07-17 14:19:51 -0700

    add models

--- app/src/main/java/com/example/team_project/model/User.java
+++ app/src/main/java/com/example/team_project/model/User.java
@@ -1,32 +1,63 @@
 package com.example.team_project.model;
 
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-// @Parcel
-public class User {
+import java.util.ArrayList;
+
+@ParseClassName("User")
+public class User extends ParseObject {
     // list the attributes
-    public String username;
-    public String password;
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_LIKED_EVENTS = "likedEvents";
+    public static final String KEY_ADDED_EVENTS = "addedEvents";
+    public static final String KEY_PROFILE_PIC = "profilePic";
+    // TODO: initialize all survey items as booleans
 
-
-    //deserialize the JSON
-    public static User fromJSON(JSONObject json) throws JSONException {
-        User user = new User();
-
-        //extract and fill values
-        user.username = json.getString("username");
-        user.password = json.getString("password");
-
-        return user;
+    public String getName() {
+        return getString(KEY_NAME);
+    }
+    public void setName(String name) {
+        put(KEY_NAME, name);
+    }
+    public String getEmail() {
+        return getString(KEY_EMAIL);
+    }
+    public void setEmail(String email) {
+        put(KEY_NAME, email);
+    }
+    public String getPassword() {
+        return getString(KEY_PASSWORD);
+    }
+    public void setPassword(String password) {
+        put(KEY_NAME, password);
+    }
+    public ArrayList<ParseObject> getLikedEvents() {
+        return (ArrayList<ParseObject>) get(KEY_LIKED_EVENTS);
+    }
+    public void setLikedEvents(ArrayList<ParseObject> likedEvents) {
+        put(KEY_LIKED_EVENTS, likedEvents);
+    }
+    public ArrayList<ParseObject> getAddedEvents() {
+        return (ArrayList<ParseObject>) get(KEY_ADDED_EVENTS);
+    }
+    public void setAddedEvents(ArrayList<ParseObject> addedEvents) {
+        put(KEY_ADDED_EVENTS, addedEvents);
+    }
+    public ParseFile getImage() {
+        return getParseFile(KEY_PROFILE_PIC);
+    }
+    public void setImage(ParseFile profilePic) {
+        put(KEY_PROFILE_PIC, profilePic);
     }
 
-    public String setUsername() {
-        return username;
-    }
-
-    public String setPassword() {
-        return password;
-    }
 
 }
