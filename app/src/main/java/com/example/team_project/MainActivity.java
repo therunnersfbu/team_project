8f84adc4c1f4e573c1f930bc02b71849c3b36a17
miguel0 <miguelbarroe@gmail.com>
2019-07-18 12:11:46 -0700

    places api working (can get details)

--- app/src/main/java/com/example/team_project/MainActivity.java
+++ app/src/main/java/com/example/team_project/MainActivity.java
@@ -7,14 +7,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.team_project.api.EventsApi;
 import com.example.team_project.api.PlacesApi;
+import com.example.team_project.model.Place;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.toolbar_main) Toolbar toolbar;
+
+//     for testing
+//     static JSONArray array;
+//     static PlacesApi api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +37,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+//        for testing
+//        MainActivity.api = new PlacesApi();
+//        MainActivity.api.setRadius(3000);
+//        MainActivity.api.setLocation(37.367890, -122.036905);
+//        MainActivity.api.getTopPlaces();
     }
+
+//    for testing
+//    public static void setArray(JSONArray array) throws JSONException {
+//        MainActivity.array = array;
+//        Place place = Place.placeFromJson(array.getJSONObject(17));
+//        MainActivity.api.setDetails(place);
+//    }
 }
