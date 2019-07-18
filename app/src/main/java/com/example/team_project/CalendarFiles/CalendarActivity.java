bcde5c211b14b2228a175159ac00d4ef3b2066d5
Shannon Jordan <shannonmj@fb.com>
2019-07-17 17:55:00 -0700

    implementing the calendar

---
+++ app/src/main/java/com/example/team_project/CalendarFiles/CalendarActivity.java
@@ -0,0 +1,15 @@
+package com.example.team_project.CalendarFiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.team_project.R;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
---
+++ app/src/main/java/com/example/team_project/CalendarFiles/CustomCalendarView.java
@@ -0,0 +1,80 @@
+package com.example.team_project.CalendarFiles;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.team_project.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class CustomCalendarView extends LinearLayout {
+    ImageButton NextButton, PreviousButton;
+    TextView CurrentDate;
+    GridView gridView;
+    private static final int MAX_CALENDAR_DAYS = 42;
+    Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    Context context;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMM YYY", Locale.ENGLISH);
+    SimpleDateFormat monthFormat = new SimpleDateFormat("MMM", Locale.ENGLISH);
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyy", Locale.ENGLISH);
+
+    List<Date> dates = new ArrayList<>();
+    List<Events> eventsList = new ArrayList<>();
+
+    public CustomCalendarView(Context context) {
+        super(context);
+    }
+
+    public CustomCalendarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        InitializeLayout();
+        SetUpCalendar();
+        PreviousButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH, -1);
+                SetUpCalendar();
+            }
+        });
+
+        NextButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH, 1);
+                SetUpCalendar();
+            }
+        });
+    }
+
+    public CustomCalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void InitializeLayout() {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.activity_calendar, this);
+        NextButton = view.findViewById(R.id.nextBtn);
+        PreviousButton = view.findViewById(R.id.previousBtn);
+        CurrentDate = view.findViewById(R.id.current_Date);
+        gridView = view.findViewById(R.id.gridview);
+
+    }
+
+    private void SetUpCalendar() {
+        String currentDate = dateFormat.format(calendar.getTime());
+        CurrentDate.setText(currentDate);
+    }
+}
