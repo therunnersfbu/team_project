7cd38ed39fc775524765e3f5ca0ca103b4cfd361
Sophia Zambri <sophiazambri@fb.com>
2019-07-17 17:12:34 -0700

    events page skeleton complete

--- app/src/main/java/com/example/team_project/fragments/EventsFragment.java
+++ app/src/main/java/com/example/team_project/fragments/EventsFragment.java
@@ -5,28 +5,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 
+import com.example.team_project.CardViewAdapter;
+import com.example.team_project.MainActivity;
 import com.example.team_project.R;
+
+import java.util.ArrayList;
+
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class EventsFragment extends Fragment {
     private Unbinder unbinder;
 
-    Context context;
-    SearchView svSearch;
-    RecyclerView rvEvents;
+    private Context context;
+    private SearchView svSearch;
+    private RecyclerView rvSuggested;
+    private ArrayList<String> names;
+    private int position;
+    private CardViewAdapter adapter;
+    RecyclerView.LayoutManager myManager;
+    LinearLayoutManager horizontalLayout;
+    View childView;
+
 
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_events, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_events, parent, false);
         unbinder = ButterKnife.bind(this, view);
         return view;
 
@@ -35,6 +48,28 @@ public class EventsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        rvSuggested = view.findViewById(R.id.rvSuggestions);
+        myManager = new LinearLayoutManager(getContext());
+        rvSuggested.setLayoutManager(myManager);
+        addItems();
+        adapter = new CardViewAdapter(names);
+        horizontalLayout = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        rvSuggested.setLayoutManager(horizontalLayout);
+        rvSuggested.setAdapter(adapter);
+
+
+    }
+
+    private void addItems() {
+
+        names = new ArrayList<>();
+        names.add("ONE");
+        names.add("TWO");
+        names.add("THREE");
+        names.add("FOUR");
+        names.add("FIVE");
+        names.add("SIX");
+
     }
 
     @Override
