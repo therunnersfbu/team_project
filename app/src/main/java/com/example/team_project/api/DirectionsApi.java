a5ae632e5b6ed015f2564733dc4ef28c7c32b247
Shannon Jordan <shannonmj@fb.com>
2019-08-01 12:18:39 -0700

    map retrieves distance

--- app/src/main/java/com/example/team_project/api/DirectionsApi.java
+++ app/src/main/java/com/example/team_project/api/DirectionsApi.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.example.team_project.account.ProfileActivity;
 import com.example.team_project.calendar.CalendarAdapter;
 import com.example.team_project.fragments.EventsFragment;
+import com.example.team_project.fragments.MapFragment;
 import com.example.team_project.search.SearchActivity;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -63,6 +64,8 @@ public class DirectionsApi {
                         ((ProfileActivity) source).gotDistances(distances);
                     } else if (source instanceof CalendarAdapter) {
                         ((CalendarAdapter) source).gotDistance(distances.get(0));
+                    } else if (source instanceof MapFragment) {
+                        ((MapFragment) source).gotDistance(distances.get(0));
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
