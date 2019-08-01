2e850ab4ac523a00a45f6a43ab855874d1c3246d
Shannon Jordan <shannonmj@fb.com>
2019-08-01 11:38:38 -0700

    polishing code

--- app/src/main/java/com/example/team_project/PublicVariables.java
+++ app/src/main/java/com/example/team_project/PublicVariables.java
@@ -1,4 +1,20 @@
 package com.example.team_project;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 public class PublicVariables {
+
+    public static String splitindicator = "\\(\\)";
+    public ArrayList<String> events;
+    public static Boolean spotType;
+
+    public static TextView tvEventName;
+    public static TextView tvAddress;
+    public static TextView tvType;
+    public static ImageView ivEventImage;
+
+
 }
