c0537188b2aa43d6a5f0c5dcc8e065164473c50c
Sophia Zambri <sophiazambri@fb.com>
2019-08-02 10:25:19 -0700

    search from searchbar in event categories

--- app/src/main/java/com/example/team_project/location/LocationActivity.java
+++ app/src/main/java/com/example/team_project/location/LocationActivity.java
@@ -6,12 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.example.team_project.R;
 import com.example.team_project.api.AutocompleteApi;
+import com.example.team_project.search.SearchActivity;
+
 import java.util.ArrayList;
 
 // used to change the current location to a location of the user's choosing.
@@ -57,6 +58,7 @@ public class LocationActivity extends AppCompatActivity implements LocationAdapt
                 mLocations.clear();
                 mLocIds.clear();
                 populateList();
+                SearchActivity.hideKeyboard(LocationActivity.this);
             }
         });
     }
