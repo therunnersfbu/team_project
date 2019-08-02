e3d46df477ee3b2309b95891777250d52fa08e7c
Shannon Jordan <shannonmj@fb.com>
2019-08-02 12:00:19 -0700

    changed profile pic

--- app/src/main/java/com/example/team_project/account/ProfileActivity.java
+++ app/src/main/java/com/example/team_project/account/ProfileActivity.java
@@ -12,13 +12,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.os.Parcelable;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -27,9 +27,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
 import com.example.team_project.BottomNavActivity;
-import com.example.team_project.LoginActivity;
 import com.example.team_project.PublicVariables;
 import com.example.team_project.R;
 import com.example.team_project.SurveyActivity;
@@ -43,9 +43,11 @@ import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
 import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -110,11 +112,7 @@ public class ProfileActivity extends AppCompatActivity implements DirectionsApi.
                     .error(defaultPic)
                     .into(ivProfilePic);
         } else {
-            Glide.with(this)
-                    .load(defaultPic)
-                    .placeholder(defaultPic)
-                    .error(defaultPic)
-                    .into(ivProfilePic);
+            ivProfilePic.setImageResource(R.drawable.default_profile_pic);
         }
 
         getLiked();
