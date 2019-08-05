74fca9524181eede555be757fed0c1148be37605
miguel0 <miguelbarroe@gmail.com>
2019-08-05 09:41:59 -0700

    made constants static

--- app/src/main/java/com/example/team_project/Constants.java
+++ app/src/main/java/com/example/team_project/Constants.java
@@ -1,6 +1,6 @@
 package com.example.team_project;
 
 public class Constants {
-    public final String splitindicator = "\\(\\)";
-    public final String separator = "()";
+    public final static String splitindicator = "\\(\\)";
+    public final static String separator = "()";
 }
